3cdb3ee228e91740a2af12f5ce20a630839353b1
vienthanhluong <69156580+vienthanhluong@users.noreply.github.com>
2026-06-02 15:22:52 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = 'stride-v1';
+const CACHE = 'stride-v2';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
